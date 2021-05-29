c69a57c60ce3752cbfb4c0861925179e1599c093
Salih Guler <muhammedsalihguler@gmail.com>
2021-05-29 11:29:55 +0300

    Fix the Dockerfile with git.

--- backend/lib/functions.dart
+++ backend/lib/functions.dart
@@ -1,6 +1,9 @@
 import 'package:functions_framework/functions_framework.dart';
 import 'package:core/core.dart';
 
+// Export api_types so builder can use them when generating `bin/server.dart`.
+export 'package:core/core.dart';
+
 @CloudFunction()
 TodoResponse function(TodoRequest request, RequestContext context) {
   listItems.add(
